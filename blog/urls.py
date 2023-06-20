9c000a78befaf6ba79c54c841f44c1caa74d7e92
Artem <artem.kravchuk.2010@mail.ru>
2023-06-21 01:27:46 +0300

    add contact

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 from .views import *
 urlpatterns = [
     path('', page_handler),
+    path('contact', page_handler_contact),
 ]
--- blog/views.py
+++ blog/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def page_handler(request):
     return render(request, 'blog2.html')
+
+def page_handler_contact(request):
+    return render(request, 'contact.html')
