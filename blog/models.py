d13322faa73de461919d57074052480636f7f279
Artem <artem.kravchuk.2010@mail.ru>
2023-06-23 03:08:17 +0300

    add models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,54 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    is_activ = models.BooleanField()
+    created_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.username
+class Post(models.Model):
+    class Status(models.TextChoices):
+        DRAFT = 'DF', 'Draft'
+        PUBLISHED = 'PB', 'Published'
+    title = models.CharField(max_length=25)
+    slug = models.SlugField(max_length=50, unique=True)
+    author = models.ForeignKey('User', on_delete=models.CASCADE)
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+    created_time = models.DateTimeField(auto_now_add=True)
+    updated_time = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=2,
+                              choices=Status.choices,
+                              default=Status.DRAFT)
+    class Meta:
+        ordering = ['-publish']
+        indexes = [
+            models.Index(fields=['-publish']),
+        ]
+    def __str__(self):
+        return self.title
+
+
+
+class Role(models.Model):
+    class Role(models.TextChoices):
+        USER = 'US', 'User'
+        ADMIN = 'AD', 'Admin'
+    role = models.CharField(max_length=2,
+                              choices=Role.choices,
+                              default=Role.USER, unique=True)
+
+    def __str__(self):
+        return self.role
+
+class User_role(models.Model):
+    user_id = models.OneToOneField('User', on_delete=models.CASCADE, primary_key=True)
+    role_id = models.OneToOneField('Role', on_delete=models.CASCADE)
+
+
