c17bf119ebcbd5c4835d1fdabbc16ea02690c845
Artem <artem.kravchuk.2010@mail.ru>
2023-06-27 02:59:45 +0300

    update templates

--- blog/models.py
+++ blog/models.py
@@ -12,7 +12,14 @@ class User(models.Model):
 
     def __str__(self):
         return self.username
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status=Post.Status.PUBLISHED)
 class Post(models.Model):
+
+    DoesNotExist = None
+
     class Status(models.TextChoices):
         DRAFT = 'DF', 'Draft'
         PUBLISHED = 'PB', 'Published'
@@ -26,6 +33,9 @@ class Post(models.Model):
     status = models.CharField(max_length=2,
                               choices=Status.choices,
                               default=Status.DRAFT)
+
+    objects = models.Manager()  # менеджер, применяемый по умолчанию
+    published = PublishedManager()  # конкретно-прикладной менеджер
     class Meta:
         ordering = ['-publish']
         indexes = [
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
-
+from .models import Post
+from django.http import Http404
+def post_detail(request, id):
+    try:
+        post = Post.published.get(id=id)
+    except Post.DoesNotExist:
+        raise Http404("No Post found.")
+    return render(request,
+            'blog/post/post.html',
+            {'post': post})
+def post_list(request):
+    posts = Post.published.all()
+    return render(request,
+        'mysite/templates/blog2.html',
+        {'posts': posts})
 # Create your views here.
 def page_handler(request):
     return render(request, 'blog2.html')
