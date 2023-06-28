e8465eb4f38ed755271f6ed1bf62bf3e1aa8d38d
Artem <artem.kravchuk.2010@mail.ru>
2023-06-28 23:09:12 +0300

    add views

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, User, Role, User_role
 
 admin.site.register(Post)
+admin.site.register(User)
+admin.site.register(Role)
+admin.site.register(User_role)
 # Register your models here.
