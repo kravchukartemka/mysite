8d9f00e91fa30a6a7c86eac5fdbf821f5d2bbe3a
Artem <artem.kravchuk.2010@mail.ru>
2023-07-04 12:00:00 +0300

    add registr

--- blog/urls.py
+++ blog/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 from .views import *
 urlpatterns = [
     path('', post_list, name='post_list'),
-    path('<int:id>/', post_detail, name='post_detail'),
     path('contact', page_handler_contact),
     path('home', page_handler_home),
     path('blog', post_list),
-    path('about', page_handler_about)
+    path('about', page_handler_about),
+    path('blog/<int:id>/', post_detail),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,17 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post
+from .form import UserForm
 from django.http import Http404
 def post_detail(request, id):
-    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
+    post = Post.objects.get(id=id)
     return render(request, 'post.html', {'post': post})
+
+def post(self, request):
+    form = UserForm(request.POST)
+    if form.is_valid():
+        form = form.save(commit=False)
+        form.save()
+        return render(request, 'registration.html')
 def post_list(request):
     posts = Post.published.all()
     return render(request,
